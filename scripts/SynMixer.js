82b2b92589d4a0d48d40e634214fd8a1fd8586c6
Anthony van Winkle <anthony@pellego.com>
2016-08-24 01:06:20 -0700

    Controller UX for current playing tracks

--- scripts/SynMixer.js
+++ scripts/SynMixer.js
@@ -55,7 +55,7 @@ function SynMixer(SynProject,$interval,$timeout) {
     }
 
     Channel.prototype.isAvailable = function() {
-        return !this.player_.src || this.player_.ended || !this.is_playing;
+        return !this.player_.src || this.player_.ended || !this.is_active;
     };
 
     Channel.prototype.loadCue = function(cue) {
@@ -68,25 +68,61 @@ function SynMixer(SynProject,$interval,$timeout) {
         // Set volume, unless we have a fadeIn
         this.player_.volume = this.fadeIn ? 0 : 1;
         // Occupied!
-        this.is_playing = true;
+        this.is_active = true;
         this.media.is_playing = true;
+
+        window.p = this.player_;
+
+        // Listen for ready
+        this.player_.oncanplay = function() {
+            $timeout(function() {
+                this.duration = this.player_.duration;
+            }.bind(this),0);
+        }.bind(this);
 
         // Listen for changes
         this.player_.onpause = function(state) {
             $timeout(function() {
-                this.is_playing = false;
                 this.media.is_playing = false;
             }.bind(this),0);
         }.bind(this);
 
+        // When the track comes to and end?
+        this.player_.onended = function() {
+            this.is_active = false;
+        }.bind(this);
+
         this.player_.play();
 
+        var elapsedCounter = $interval(function() {
+            this.currentTime = this.player_.currentTime;
+            // Cancel if we're not playing anymore
+            if (!this.is_active) {
+                $interval.cancel(elapsedCounter);
+            }
+        }.bind(this),10);
+
+    };
+
+    Channel.prototype.pause = function() {
+        this.player_.pause();
+        this.media.is_playing = false;
+    };
+
+    Channel.prototype.play = function() {
+        this.player_.play();
+        this.media.is_playing = true;
+    };
+
+    Channel.prototype.setTime = function() {
+        console.log("setting time!")
+        this.player_.currentTime = this.currentTime;
     };
 
     Channel.prototype.stop = function() {
 
         // We might not need to
-        if (!this.is_playing) { return; }
+        if (!this.is_active) { return; }
 
         // Fade out somehow
         var fadepct = this.fadeOut;
@@ -105,7 +141,7 @@ function SynMixer(SynProject,$interval,$timeout) {
 
             if (this.player_.volume === 0) {
                 this.player_.pause();
-                this.is_playing = false;
+                this.is_active = false;
                 this.media.is_playing = false;
                 $interval.cancel(fadeAudio);
             }
@@ -114,9 +150,10 @@ function SynMixer(SynProject,$interval,$timeout) {
 
     };
 
-    function Group(groupname) {
+    function Group(groupname,mixer) {
         this.name = groupname;
         this.channels = [];
+        this.mixer_ = mixer;
 
         // Start with a default channel
         this.addChannel();
@@ -130,7 +167,10 @@ function SynMixer(SynProject,$interval,$timeout) {
             fadeOut: 2000,
         });
 
+        // Store a pointer in this group's channels list
         this.channels.push(ch);
+        // Store a pointer in the master mixer channels list
+        this.mixer_.channels.push(ch);
 
         return ch;
     };
@@ -177,15 +217,14 @@ function SynMixer(SynProject,$interval,$timeout) {
         // The groups, including defaults
         this.groups = {
             // SOLO_  : new Group('SOLO_'),
-            MUSIC_ : new Group('MUSIC_'),
-            COMMON_: new Group('COMMON_'),
+            MUSIC_ : new Group('MUSIC_',this),
+            COMMON_: new Group('COMMON_',this),
         };
 
         return this;
     }
 
     Mixer.prototype.play = function(cue) {
-        console.log("PLAYING: " + cue.file);
 
         // Is there a group for this button?
         if (cue.group) {
@@ -195,7 +234,7 @@ function SynMixer(SynProject,$interval,$timeout) {
 
             // Does this group need to be created?
             if (!this.groups.hasOwnProperty(gname)) {
-                this.groups[gname] = new Group(gname);
+                this.groups[gname] = new Group(gname,this);
             }
             this.groups[gname].play(cue);
 
